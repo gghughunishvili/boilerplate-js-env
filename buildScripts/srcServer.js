c0a07217733d290f991d043233c19f6a1806692c
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2020-07-25 19:50:06 +0400

    added fetching basic users list;

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,13 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', (req, res) => {
+  res.json([
+    {id: 1, firstName: "John", lastName: "Doe", email: "john_doe@example.com"},
+    {id: 2, firstName: "Jane", lastName: "Doe", email: "jane_doe@example.com"}
+  ]);
+});
+
 app.listen(port, (err) => {
   if (err) {
     console.log(err); // eslint-disable-line no-console
